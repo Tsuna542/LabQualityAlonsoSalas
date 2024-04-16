3618aa7c5b4af7cc17fd6bc2aa9c2eaae3628cae
Alonso Salas <alonso.salasc@usil.pe>
2024-04-16 16:02:39 -0500

    Cambio final del labo Alonso Salas

--- prueba1.py
+++ prueba1.py
@@ -1,2 +1,3 @@
 # La prueba 1 jejeje
 # Esto es un cambio en el codigo de prueba 1 :D
+# Ultimo cambio del labo :D
--- prueba2.py
+++ prueba2.py
@@ -1,2 +1,3 @@
 # La prueba 2 jejeje
 # Esto es un cambio en el codigo de prueba 2 :D
+# Ultimo cambio del labo :D
